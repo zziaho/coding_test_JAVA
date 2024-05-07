ba19cb863d1df258ffb3842cf1469b2ef8ed95d3
ziaho <rlawlgh1100@naver.com>
2024-05-07 22:34:56 +0900

    feature / 백준 Step1 Lv1_2557

---
+++ 백준/step1/Lv1_2557.java
@@ -0,0 +1,10 @@
+package 백준.step1;
+
+public class Lv1_2557 {
+    public static void main(String[] args) {
+        // step1. 입출력과 사칙연산
+
+        // Hello World!를 출력하시오.
+        System.out.println("Hello World!");
+    }
+}
