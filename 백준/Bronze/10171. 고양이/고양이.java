a0380e44ad1c850bded25e61fc4a9844bd62d8f7
zziaho <96275070+zziaho@users.noreply.github.com>
2025-03-19 23:35:43 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+//		아래 예제와 같이 고양이를 출력하시오.
+		
+//		\    /\
+//		 )  ( ')
+//		(  /  )
+//		 \(__)|
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+//		이스케이프 문자 "\" 사용
+	}
+
+}
