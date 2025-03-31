5a84fd389ebfb246fe2e943f7863abf3d1fde9d4
zziaho <96275070+zziaho@users.noreply.github.com>
2025-03-31 22:46:13 +0900

    [Bronze V] Title: A+B - 3, Time: 116 ms, Memory: 15812 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int result = 0;
+			while (st.hasMoreTokens()) {
+				result += Integer.parseInt(st.nextToken());
+			}
+			bw.write(result + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		solution();
+	}
+}
