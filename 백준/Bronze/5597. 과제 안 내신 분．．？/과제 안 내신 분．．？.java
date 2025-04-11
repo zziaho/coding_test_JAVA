9a1bcec2ee0de783396e1c76d6bcbd3e93791ca8
zziaho <96275070+zziaho@users.noreply.github.com>
2025-04-11 20:08:39 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -7,25 +7,20 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int[] allStudent = new int[30];
-		int[] notSubmit = new int[2];
-		Arrays.setAll(allStudent, i -> i+1);
+		boolean[] submitted = new boolean[31];
 
 		for (int i = 0; i < 28; i++) {
-			int check = Integer.parseInt(br.readLine());
-
-			for (int j = 0; j < allStudent.length; j++) {
-				if (check == allStudent[j]) {
-					allStudent[j] = 0;
-				}
-			}
+			int num = Integer.parseInt(br.readLine());
+			submitted[num] = true;
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for (int k = 0; k < allStudent.length; k++) {
-			if (allStudent[k] != 0) sb.append(allStudent[k] + "\n");
+		for (int i = 1; i <= 30; i++) {
+			if (!submitted[i])
+			sb.append(i).append("\n");
 		}
-		bw.write(String.valueOf(sb.delete(sb.length()-1, sb.length())));
+
+		bw.write(String.valueOf(sb));
 
 		bw.flush();
 		bw.close();
