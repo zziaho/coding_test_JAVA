f54f2af321311d9a46dd11a430cc58f0c188a8da
zziaho <96275070+zziaho@users.noreply.github.com>
2025-04-25 21:44:51 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void solution(){
+
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+
+	}
+
+	public static void main(String[] args){
+		solution();
+	}
+}
