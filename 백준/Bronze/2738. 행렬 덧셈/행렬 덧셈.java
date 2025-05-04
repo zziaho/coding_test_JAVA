56b84192c4380edd47cfa4b7cf13a4b3d12b848d
zziaho <96275070+zziaho@users.noreply.github.com>
2025-05-05 02:19:30 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 200 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] a = new int[n][m];
+		int[][] b = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				b[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				bw.write(String.valueOf(a[i][j] + b[i][j] + " "));
+			}
+			bw.write("\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		solution();
+	}
+}
